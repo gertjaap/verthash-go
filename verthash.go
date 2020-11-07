e3aba50f5bdd5c9ac471406129908a8db686da80
Gert-Jaap Glasbergen <gertjaap@gertjaap.org>
2020-11-07 21:50:04 +0100

    Initial commit

---
+++ verthash.go
@@ -0,0 +1,134 @@
+package verthash
+
+import (
+	"bytes"
+	"encoding/binary"
+	"io/ioutil"
+	"os"
+
+	"golang.org/x/crypto/sha3"
+)
+
+const VerthashHeaderSize uint32 = 80
+const VerthashHashOutSize uint32 = 32
+const VerthashP0Size uint32 = 64
+const VerthashIter uint32 = 8
+const VerthashSubset uint32 = VerthashP0Size * VerthashIter
+const VerthashRotations uint32 = 32
+const VerthashIndexes uint32 = 4096
+const VerthashByteAlignment uint32 = 16
+
+func MakeVerthashDatafile(file string) error {
+	pk := sha3.Sum256([]byte("Verthash Proof-of-Space Datafile"))
+	NewGraph(17, file, pk[:])
+
+	return nil
+}
+
+type Verthash struct {
+	datFileContent []byte
+	datFile        *os.File
+}
+
+func fnv1a(a, b uint32) uint32 {
+	return (a ^ b) * 0x1000193
+}
+
+func NewVerthash(datFileLocation string, keepInRam bool) (*Verthash, error) {
+	v := Verthash{}
+	var err error
+	if keepInRam {
+		v.datFileContent, err = ioutil.ReadFile(datFileLocation)
+	} else {
+		v.datFile, err = os.Open(datFileLocation)
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return &v, nil
+}
+
+func (v *Verthash) Close() {
+	if v.datFileContent != nil {
+		v.datFileContent = []byte{}
+	} else {
+		v.datFile.Close()
+	}
+}
+
+func (v *Verthash) SumVerthash(input []byte) ([32]byte, error) {
+	p1 := [32]byte{}
+
+	inputCopy := make([]byte, len(input))
+	copy(inputCopy[:], input[:])
+	sha3hash := sha3.Sum256(inputCopy)
+
+	copy(p1[:], sha3hash[:])
+	p0 := make([]byte, VerthashSubset)
+	for i := uint32(0); i < VerthashIter; i++ {
+		inputCopy[0] += 0x01
+		digest64 := sha3.Sum512(inputCopy)
+		copy(p0[i*VerthashP0Size:], digest64[:])
+	}
+
+	buf := bytes.NewBuffer(p0)
+	p0Index := make([]uint32, len(p0)/4)
+	for i := 0; i < len(p0Index); i++ {
+		binary.Read(buf, binary.LittleEndian, &p0Index[i])
+	}
+
+	seekIndexes := make([]uint32, VerthashIndexes)
+
+	for x := uint32(0); x < VerthashRotations; x++ {
+		copy(seekIndexes[x*VerthashSubset/4:], p0Index)
+		for y := 0; y < len(p0Index); y++ {
+			p0Index[y] = (p0Index[y] << 1) | (1 & (p0Index[y] >> 31))
+		}
+	}
+
+	var datFileSize int64
+
+	if v.datFileContent == nil {
+		s, err := v.datFile.Stat()
+		if err != nil {
+			return [32]byte{}, err
+		}
+		datFileSize = s.Size()
+	} else {
+		datFileSize = int64(len(v.datFileContent))
+	}
+
+	var valueAccumulator uint32
+	var mdiv uint32
+	mdiv = ((uint32(datFileSize) - VerthashHashOutSize) / VerthashByteAlignment) + 1
+	valueAccumulator = uint32(0x811c9dc5)
+	buf = bytes.NewBuffer(p1[:])
+	p1Arr := make([]uint32, VerthashHashOutSize/4)
+	for i := 0; i < len(p1Arr); i++ {
+		binary.Read(buf, binary.LittleEndian, &p1Arr[i])
+	}
+	for i := uint32(0); i < VerthashIndexes; i++ {
+		offset := (fnv1a(seekIndexes[i], valueAccumulator) % mdiv) * VerthashByteAlignment
+
+		data := make([]byte, 32)
+		if v.datFileContent != nil {
+			data = v.datFileContent[offset : offset+VerthashHashOutSize]
+		} else {
+			v.datFile.Seek(int64(offset), 0)
+			v.datFile.Read(data)
+		}
+
+		for i2 := uint32(0); i2 < VerthashHashOutSize/4; i2++ {
+			value := binary.LittleEndian.Uint32(data[i2*4 : ((i2 + 1) * 4)])
+			p1Arr[i2] = fnv1a(p1Arr[i2], value)
+			valueAccumulator = fnv1a(valueAccumulator, value)
+		}
+	}
+
+	for i := uint32(0); i < VerthashHashOutSize/4; i++ {
+		binary.LittleEndian.PutUint32(p1[i*4:], p1Arr[i])
+	}
+
+	return p1, nil
+}
---
+++ verthash_test.go
@@ -0,0 +1,65 @@
+package verthash
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+var verthashTestInput []byte
+var verthashTestOutput []byte
+
+func TestMain(m *testing.M) {
+	fmt.Println("Verthash test vectors require generation of Verthash file, this will be slow!")
+	os.Remove("/tmp/verthash_verthash_go_testsuite.dat")
+	MakeVerthashDatafile("/tmp/verthash_verthash_go_testsuite.dat")
+	verthashTestInput, _ = hex.DecodeString("000000203a297b4b7685170d7644b43e5a6056234cc2414edde454a87580e1967d14c1078c13ea916117b0608732f3f65c2e03b81322efc0a62bcee77d8a9371261970a58a5a715da80e031b02560ad8")
+	verthashTestOutput, _ = hex.DecodeString("E0F6C10B4A38F35A6CDCC26D32A7ED8C3BFC5D827A9BC72647AFA324B70D0463")
+	code := m.Run()
+	os.Remove("/tmp/verthash_verthash_go_testsuite.dat")
+	os.Exit(code)
+}
+
+func TestCreation(t *testing.T) {
+	b, err := ioutil.ReadFile("/tmp/verthash_verthash_go_testsuite.dat")
+	if err != nil {
+		t.Error(err)
+	}
+	hash := sha256.Sum256(b)
+	expectedHash, _ := hex.DecodeString("a55531e843cd56b010114aaf6325b0d529ecf88f8ad47639b6ededafd721aa48")
+	if !bytes.Equal(hash[:], expectedHash) {
+		t.Errorf("Generated file has wrong hash: %x vs %x", hash, expectedHash)
+	}
+}
+
+func TestHashDisk(t *testing.T) {
+	vh, err := NewVerthash("/tmp/verthash_verthash_go_testsuite.dat", false)
+	if err != nil {
+		t.Error(err)
+	}
+	defer vh.Close()
+
+	h, err := vh.SumVerthash(verthashTestInput)
+	if !bytes.Equal(h[:], verthashTestOutput) {
+		t.Errorf("Verthash output invalid: %x vs %x", h, verthashTestOutput)
+	}
+
+}
+
+func TestHashMemory(t *testing.T) {
+	vh, err := NewVerthash("/tmp/verthash_verthash_go_testsuite.dat", true)
+	if err != nil {
+		t.Error(err)
+	}
+	defer vh.Close()
+
+	h, err := vh.SumVerthash(verthashTestInput)
+	if !bytes.Equal(h[:], verthashTestOutput) {
+		//os.Remove("/tmp/verthash.dat")
+		t.Errorf("Verthash output invalid: %x vs %x", h, verthashTestOutput)
+	}
+}
